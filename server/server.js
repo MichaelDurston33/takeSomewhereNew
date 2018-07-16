e8137cbebba6a71547fd7051e597e5d7fc053f90
Michael Durston <michaeldurston@michaels-air.home>
2018-07-16 23:27:03 +0100

    setup start script for heroku

--- server/server.js
+++ server/server.js
@@ -1,19 +1,20 @@
 var express = require("express");
 var app = express();
 var path = require('path');
-var cors = require('cors')
+
+const port = process.env.PORT || 3000;
+
 
 // viewed at http://localhost:8080
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/../public/homepage.html'));
 });
 
-app.listen(3000, () => {
-  console.log("Server listening on port 3000")
+app.listen(port, () => {
+  console.log("Server listening on port " + port)
 });
 
 app.use(express.static('public'));
-app.use(cors())
 
 // app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({ extended: true }));
